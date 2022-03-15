6d2f5137ff59bde816ccc35189b1493fa059804d
paulleerentveld <paul@veldnet.com.au>
2022-03-15 16:38:04 +1000

    add functions for lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+const driver={};
+
+
+function updateDriverWithKeyAndValue(driver,key,value) {
+    let newDriver = Object.assign({...driver},{[key]:value});
+    return newDriver;
+
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver,key,value) {
+    Object.assign(driver,{[key]:value});
+    return driver;
+}
+
+function deleteFromDriverByKey(obj,key) {
+    let newDriver = Object.assign({},obj);
+    delete newDriver[key];
+    return newDriver;
+}
+
+function destructivelyDeleteFromDriverByKey(obj,key) {
+    delete obj[key];
+    return obj;
+}
+
